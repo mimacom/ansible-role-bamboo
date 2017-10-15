5718caef3bd3423569d6025fd983c5d645780df2
Remo Wenger <privat@remowenger.ch>
2017-10-15 19:05:23 +0200

    Clean up old files

--- files/tmp_cleanup.sh
+++
@@ -1,8 +0,0 @@
-CLEANUPDIR=/tmp/
-
-MAXAGE=+1
-
-echo "  removing files from $CLEANUPDIR older than $MAXAGE days from folders:"
-find $CLEANUPDIR  -maxdepth 1 -type d -mtime $MAXAGE | xargs rm -rf -v 2>/dev/null
-echo "  file cleanup done."
-
